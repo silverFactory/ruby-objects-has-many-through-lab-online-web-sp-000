31a3ff2fe1f4c9dcac64d00d3656481a746cbe30
Will Studabaker <will.studabaker@gmail.com>
2020-11-03 18:07:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,11 @@
 class Song
-
+  attr_accessor :name, :artist, :genre
   @@all = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
   def self.all
     @@all
   end
